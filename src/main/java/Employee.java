289e2de803258ab7f8919b0e82b5ac98d0ff2a5c
EMILY GAVIN - STUDENT <G00382828@atu.ie>
2022-12-07 09:35:35 +0000

    Main file added

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,7 +1,47 @@
 public class Employee {
-    private String name;
-    private String pss;
+
+    String name;
+    String pps;
+
+    public Employee() {
+    }
+
+    public Employee(String name, String pps) {
+        this.name = name;
+        this.pps = pps;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if(name.length() < 5 || name.length() > 22){
+            System.out.println("Invalid name entered");
+        }
+        else {
+            this.name = name;
+        }
+    }
+
+    public String getPps() {
+        return pps;
+    }
+
+    public void setPps(String pps) {
+        if(pps.length() == 6) {
+            this.pps = pps;
+        }
+        else{
+            System.out.println("Invalid PPS entered (Must be 6 characters)");
+        }
+    }
 }
+
+
+
+
+
 
 
 
